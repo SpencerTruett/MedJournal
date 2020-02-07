9037d2aa053627f16dee1e74d3c0e9ffeb57d432
Spencer Truett <srtruett1993@gmail.com>
2020-02-07 13:51:55 -0600

    added activity list

---
+++ src/components/activity/ActivityList.js
@@ -0,0 +1,27 @@
+import React, { useContext } from "react"
+import "./Activity.css"
+import { ActivityContext } from "./ActivityProvider"
+import Activity from "./Activity"
+
+export default (props) => {
+    const { Acts } = useContext(ActivityContext)
+    // const activeUserId = parseInt(localStorage.getItem("activeUser"), 10)
+
+
+    return (
+        <>
+        <div className="ActivityList">
+            <div>
+                <h1>Activity Records</h1>
+            </div>    
+            <div className="ActivityReadings">
+                {
+                    Acts.map(Act => {
+                        return <Activity key={Act.id} Act={Act} {...props} />
+                    })
+                }
+            </div>
+        </div>
+        </>
+    )
+}
